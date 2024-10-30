c5cbb7d9e58a7a6b1eef13f1d3bb08ec68d975ee
harshilpatel8819 <harshilpatel8819@gmail.com>
2024-10-30 15:46:42 +0530

    fix deploy build issue

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
 // App.test.tsx
-import React from "react";
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import { vi, describe, it, expect, beforeEach } from "vitest";
 import App from "./App";
